f4ae3163f6d8a6f1d34d46d7311b80d8a38b54d1
Ubuntu <ubuntu@DockerTest.2bz343c5ps0u3gtjogwjs3khnf.parx.internal.cloudapp.net>
2023-02-17 11:22:24 +0000

    pagina basica operativa

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+  return (
+    <div>
+      <h1>Hola, mundo!</h1>
+      <p>Esta es mi primera página con React.</p>
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
