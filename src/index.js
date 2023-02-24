2fdaf7a2fdeb9ea6bfc59b10b17ba2767018991d
jgarcialogic <118981008+jgarcialogic@users.noreply.github.com>
2023-02-24 15:06:27 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
 function App() {
+  const [contador, setContador] = useState(0);
+
+  function incrementarContador() {
+    setContador(contador + 1);
+  }
+
   return (
     <div>
       <h1>Hola, mundo!</h1>
       <p>Esta es mi primera página con React.</p>
+      <Contador valor={contador} incrementar={incrementarContador} />
+    </div>
+  );
+}
+
+function Contador(props) {
+  return (
+    <div>
+      <p>El valor del contador es: {props.valor}</p>
+      <button onClick={props.incrementar}>Incrementar</button>
     </div>
   );
 }
